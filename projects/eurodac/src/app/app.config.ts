fc1d5401e8d08320ac73d1eb2af43721a72bfbbc
Joan Margineda <joan.margineda@bioidenti.com>
2025-11-14 10:37:21 +0100

    Migrate global layout components and styles into shared library

--- projects/eurodac/src/app/app.config.ts
+++ projects/eurodac/src/app/app.config.ts
@@ -1,12 +1,42 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+
+import Aura from '@primeuix/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { UserRepositoryService } from '@shared/services/user-repository.service';
+import { UserRepositoryMockService } from '@shared/services/mock/user-repository-mock.service';
+import { ConfigurationService } from '@shared/services/configuration.service';
+import { ConfigurationFileService } from './shared/services/configuration-file.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: Aura } }),
+    provideAppInitializer(() => {
+      const configService = inject(ConfigurationFileService);
+      return configService.initialize();
+    }),
+    {
+      provide: UserRepositoryService,
+      useClass: UserRepositoryMockService,
+    },
+    {
+      provide: ConfigurationService,
+      useClass: ConfigurationFileService,
+    },
+  ],
 };
--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -3,7 +3,7 @@
  */
 
 export * from './lib/shared';
-
+export * from './lib/layout/app.layout';
 export * from './lib/services/configuration.service';
 export * from './lib/services/user-repository.service';
 export * from './lib/services/mock/user-repository-mock.service';
